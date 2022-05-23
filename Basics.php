db91ec9ff19900f3a1f4732ba5927e2137155d34
jawad khan <73393333+jkbells@users.noreply.github.com>
2022-05-23 23:53:07 +0500

    files html and css

---
+++ Basics.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Document</title>
+</head>
+<body>
+    <div class = "container">
+        This is my first php website
+</div>
+
+<?php 
+    echo "Hello world and this is printed using php";
+
+    $variable1 = 14;
+    $variable2 = 14;
+
+    echo $variable1;
+    echo $variable2;
+
+    echo $variable1 + $variable2;
+    
+?>
+    
+</body>
+</html> 
